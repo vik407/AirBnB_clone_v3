ff598cdcc6c61ded90ed296473dd267f4b6ac86b
Victor Hernandez <vik407@gmail.com>
2020-04-29 21:29:20 +0000

    Stats by object types

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -20,3 +20,17 @@ dcon = {
 def status():
     """Status"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def obj_num():
+    """Endpoint that retrieves the number of each objects by type"""
+    objtype = {
+        "amenities": storage.count('Amenity'),
+        "cities": storage.count('City'),
+        "places": storage.count('Place'),
+        "reviews": storage.count('Review'),
+        "states": storage.count('State'),
+        "users": storage.count('User')
+    }
+    return jsonify(objtype), 200
