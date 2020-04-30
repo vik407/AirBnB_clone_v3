665a5de66a8544dfd574630cd804d952cdc2dc41
Victor Hernandez <vik407@gmail.com>
2020-04-30 01:40:20 +0000

    Fixing states file

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -7,7 +7,7 @@ from models.state import State
 
 
 @app_views.route('/states', strict_slashes=False, methods=['GET'])
-def get_all_states()
+def get_all_states():
         "Get the states"
         return jsonify([state.to_dict() for state in
                        storage.all(State).values()])
@@ -34,6 +34,7 @@ def del_state(s_id):
                 return jsonify({}), 200
         abort(404)
 
+
 @app_views.route("/states", strict_slashes=False, methods=['POST'])
 def post_state():
         """POST a new state"""
@@ -57,7 +58,7 @@ def put_state_id(s_id):
         the_state = storage.get(State, s_id)
         if the_state is not None:
                 for attr, value in request.get_json().items():
-                setattr(the_state, attr, value)
+                        setattr(the_state, attr, value)
                 storage.save()
                 return jsonify(the_state.to_dict()), 200
         abort(404)
