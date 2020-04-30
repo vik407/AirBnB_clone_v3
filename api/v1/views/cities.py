6f94f02f3a1e0d3842af5cf4a31b3400979f50f3
daod2003 <1253@holbertonschool.com>
2020-04-30 03:09:15 +0000

    fixed pep8

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -9,7 +9,7 @@ from models.city import City
 
 
 @app_views.route('/states/<state_id>/cities', methods=['GET'],
-                strict_slashes=False)
+                 strict_slashes=False)
 def cities(state_id):
     """get all the cities in a list"""
     idstate = storage.get(State, state_id)
@@ -22,7 +22,7 @@ def cities(state_id):
 
 
 @app_views.route('/cities/<city_id>', methods=['GET'],
-                strict_slashes=False)
+                 strict_slashes=False)
 def cityid(city_id):
     idcity = storage.get(City, city_id)
     if idcity is None:
@@ -42,7 +42,7 @@ def delete(city_id):
 
 
 @app_views.route('/states/<state_id>/cities/', methods=['POST'],
-                strict_slashes=False)
+                 strict_slashes=False)
 def post(state_id):
     """function or route that create a new city"""
     idState = storage.get(State, state_id)
@@ -61,7 +61,7 @@ def post(state_id):
 
 
 @app_views.route('/cities/<city_id>', methods=['PUT'],
-                strict_slashes=False)
+                 strict_slashes=False)
 def put(city_id):
     """function or route that update a city"""
     the_json = request.get_json()
